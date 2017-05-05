2572c3e0601596bd34b7baa4823dc21728ebc4d1
admin <admin@192.168.0.2>
2017-05-05 13:41:43 +0200

    test

--- src/misteKiste/Wuerfel.java
+++ src/misteKiste/Wuerfel.java
@@ -1,5 +1,28 @@
 package misteKiste;
 
+import java.util.Random;
+
 public class Wuerfel {
+	
+	private int wert = 0;
+	private int seitenZahl = 6;
+	
+	public Wuerfel(int dieSeitenZahl){
+		seitenZahl = dieSeitenZahl;
+	}
+	
+	public int wuerfeln(){
+		Random rand = new Random();
+		wert = 1 + rand.nextInt(seitenZahl);
+		return wert;
+	}
+	
+	public int getSeitenZahl(){
+		return seitenZahl;
+	}
+	
+	public int getWert(){
+		return wert;
+	}
 
 }
